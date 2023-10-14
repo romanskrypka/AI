1a68e6d7ba701a8da4b877a45207b5b4f1a7f8e7
Roman <58446922+romanskrypka@users.noreply.github.com>
2023-10-14 12:49:57 +0300

    AI_02

---
+++ basic_py/FirstProgram.py
@@ -0,0 +1,28 @@
+def MyPrint(d, r):
+    print ('В функции мы выполнили печать ', d, 'переменньх x и y')
+    print ('Результат Z=', r)
+
+print ( "Привет Python") 
+
+
+x = 2
+y = 3
+z = x + y
+print(x, y)
+print(z)
+MyPrint ('сложения', z)
+z = y - x
+print(z)
+MyPrint ('вычитания', z)
+z = x * y
+print(z)
+MyPrint ('умножения', z)
+z = x / y
+print(z)
+MyPrint ('деления', z)
+z = x ** y # возведение в степень
+print(z)
+MyPrint ('возведения в степень', z)
+
+
+
